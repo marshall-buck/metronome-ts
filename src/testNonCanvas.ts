6ada6159e42fdfe223cc7c4af92dd80936dee815
Marshall Buck <dev@marshallbuck.com>
2023-01-15 18:13:33 -0600

    switched to svg background

--- src/testNonCanvas.ts
+++ src/testNonCanvas.ts
@@ -1,20 +1,5 @@
-/** retuns the background div */
-
-// let lastDegree=0 //degree when mousedown ended
-// let currentDegree=0  //current degree as mouse moves
-// const bg = document.createElement("div");
-// let startDragDegree;
-
-function returnBgDiv() {
-  const root = document.querySelector("#app");
-  const bg = document.createElement("div");
-
-  const center = getCenter();
-  const aspect = portOrLand(center.x, center.y);
-  bg.id = "background";
-  bg.className = aspect === "landscape" ? "bg-land" : "bg-port";
-  root?.append(bg);
-}
+const Y_OFFSET = 76;
+const X_OFFSET = 24;
 
 function createIcons() {
   const bg = document.querySelector("#background") as HTMLDivElement;
@@ -23,19 +8,19 @@ function createIcons() {
 
   const center = getCenter();
   console.log(box, center);
+  // getAbsolutePosition();
 
-  const settings = document.createElement("div");
-  settings.id = "settings";
+  const settings = createSettingsIcon();
+
   const settingsCord = {
-    top: box.top + box.height - 48,
-    left: box.left + box.width / 2 - 24,
+    bottom: 48,
+    left: box.left + box.width / 2,
   };
   settings.setAttribute(
     "style",
-    `top: ${settingsCord.top}px; left: ${settingsCord.left}px; position: absolute; `
+    `transform translate(${box.height}, ${center.x})`
   );
 
-  settings.append(createSettingsIcon());
   bg.append(settings);
 
   const timeSig = document.createElement("div");
@@ -51,10 +36,10 @@ function createIcons() {
   );
 
   timeSig.append(createTimeSigIcon());
-  bg.append(timeSig);
-  bg.append(timeSig);
+  // bg.append(timeSig);
+  // bg.append(timeSig);
 }
-
+/** returns center x y coords of window */
 function getCenter() {
   return {
     x: window.innerWidth / 2,
@@ -62,68 +47,115 @@ function getCenter() {
   };
 }
 /** Returns string portrait or landscape*/
-function portOrLand(width: number, height: number): string {
-  return height >= width ? "portrait" : "landscape";
+function portOrLand(): string {
+  return window.innerHeight >= window.innerWidth ? "portrait" : "landscape";
 }
 window.addEventListener("resize", (_e: Event) => {
   const center = getCenter();
-  const aspect = portOrLand(center.x, center.y);
-  const bg = document.querySelector("#background") as HTMLDivElement;
-  bg.className = aspect === "landscape" ? "bg-land" : "bg-port";
-  const box = bg.getBoundingClientRect();
-  console.log(box);
-  console.log(center);
+  const bgSvg = document.querySelector("#bg") as SVGElement;
+  const bgCircle = document.querySelector("#bg-circle") as SVGElement;
+  const radius =
+    portOrLand() === "portrait"
+      ? window.innerWidth / 2
+      : window.innerHeight / 2;
+  const width = window.innerWidth.toString();
+  const height = window.innerHeight.toString();
 
-  const settings = document.querySelector("#settings") as HTMLDivElement;
-  const timeSig = document.querySelector("#time-sig") as HTMLDivElement;
+  bgSvg.setAttribute("width", `${width}`);
+  bgSvg.setAttribute("height", `${height}`);
+  bgCircle.setAttribute("cx", `${center.x}`);
+  bgCircle.setAttribute("cy", `${center.y}`);
+  bgCircle.setAttribute("r", `${radius}`);
 
-  const settingsCord = {
-    top: box.top + box.height - 48,
-    left: box.left + box.width / 2 - 24,
-  };
-
-  settings.setAttribute(
-    "style",
-    `top: ${settingsCord.top}px; left: ${settingsCord.left}px; position: absolute; `
-  );
-  const timeSigCord = {
-    top: center.y - 24,
-    left: box.left,
-  };
-  timeSig.setAttribute(
-    "style",
-    `top: ${timeSigCord.top}px; left: ${timeSigCord.left}px; position: absolute; `
-  );
-  bg.className = aspect === "landscape" ? "bg-land" : "bg-port";
-  // console.log(bg.getBoundingClientRect());
+  // const center = getCenter();
+  // const aspect = portOrLand(center.x, center.y);
+  // const bg = document.querySelector("#background") as HTMLDivElement;
+  // bg.className = aspect === "landscape" ? "bg-land" : "bg-port";
+  // const box = bg.getBoundingClientRect();
+  // // console.log(box);
+  // // console.log(center);
+  // const settings = document.querySelector("#settings") as HTMLDivElement;
+  // const timeSig = document.querySelector("#time-sig") as HTMLDivElement;
+  // const settingsCord = {
+  //   top: box.top + box.height - 48,
+  //   left: box.left + box.width / 2 - 24,
+  // };
+  // settings.setAttribute(
+  //   "style",
+  //   `top: ${settingsCord.top}px; left: ${settingsCord.left}px; position: absolute; `
+  // );
+  // const timeSigCord = {
+  //   top: center.y - 24,
+  //   left: box.left,
+  // };
+  // timeSig.setAttribute(
+  //   "style",
+  //   `top: ${timeSigCord.top}px; left: ${timeSigCord.left}px; position: absolute; `
+  // );
+  // bg.className = aspect === "landscape" ? "bg-land" : "bg-port";
+  // // console.log(bg.getBoundingClientRect());
 });
 function init() {
-  returnBgDiv();
-  createIcons();
+  const root = document.querySelector("#app") as HTMLDivElement;
+
+  const svg = createBGsvg();
+  root.append(svg);
+  createSettingsIcon(svg);
+  createTimeSigIcon(svg);
+  createBPMIcon(svg);
 }
 init();
 export {};
 
-function createSettingsIcon() {
-  const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+function createSettingsIcon(parent: SVGElement) {
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
   const iconPath = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "path"
   );
+  parent.append(group);
+  const bg = document.querySelector("#bg-circle");
 
-  iconSvg.setAttribute("width", "48");
-  iconSvg.setAttribute("height", "48");
-
+  const coords = bg?.getBoundingClientRect() as DOMRect;
+  group.setAttribute("id", "settings");
+  const x = coords.width / 2 - X_OFFSET;
+  const y = coords.bottom - Y_OFFSET;
+  group.setAttributeNS(null, "transform", `translate(${x}, ${y})`);
   iconPath.setAttribute(
     "d",
     "M19.4 44l-1-6.3q-.95-.35-2-.95t-1.85-1.25l-5.9 2.7L4 30l5.4-3.95q-.1-.45-.125-1.025Q9.25 24.45 9.25 24q0-.45.025-1.025T9.4 21.95L4 18l4.65-8.2 5.9 2.7q.8-.65 1.85-1.25t2-.9l1-6.35h9.2l1 6.3q.95.35 2.025.925Q32.7 11.8 33.45 12.5l5.9-2.7L44 18l-5.4 3.85q.1.5.125 1.075.025.575.025 1.075t-.025 1.05q-.025.55-.125 1.05L44 30l-4.65 8.2-5.9-2.7q-.8.65-1.825 1.275-1.025.625-2.025.925l-1 6.3zM24 30.5q2.7 0 4.6-1.9 1.9-1.9 1.9-4.6 0-2.7-1.9-4.6-1.9-1.9-4.6-1.9-2.7 0-4.6 1.9-1.9 1.9-1.9 4.6 0 2.7 1.9 4.6 1.9 1.9 4.6 1.9z"
   );
 
-  iconSvg.appendChild(iconPath);
-  return iconSvg;
+  group.append(iconPath);
+  // return group;
 }
-function createTimeSigIcon() {
-  const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+function createBPMIcon(parent: SVGElement) {
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  const iconPath = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  parent.append(group);
+  const bg = document.querySelector("#bg-circle");
+
+  const coords = bg?.getBoundingClientRect() as DOMRect;
+
+  console.log("bpm", coords);
+  const x = coords.width - Y_OFFSET;
+  const y = coords.top + coords.height / 2 - X_OFFSET;
+
+  iconPath.setAttribute(
+    "d",
+    "M20.85 31.05q1.25 1.25 3.175 1.2 1.925-.05 2.825-1.4l11.6-17.25-17.3 11.6q-1.35.9-1.45 2.75-.1 1.85 1.15 3.1ZM9.5 40.7q-1.05 0-2-.5t-1.45-1.45q-1.4-2.45-2.1-5.15-.7-2.7-.7-5.5 0-4.35 1.625-8.125Q6.5 16.2 9.3 13.375q2.8-2.825 6.55-4.475 3.75-1.65 8.05-1.65 4.35 0 8.15 1.65 3.8 1.65 6.65 4.45 2.85 2.8 4.475 6.6 1.625 3.8 1.625 8.15-.05 2.8-.725 5.525-.675 2.725-2.025 5.125-.6.95-1.525 1.45-.925.5-1.975.5Z"
+  );
+  group.setAttributeNS(null, "transform", `translate(${x}, ${y})`);
+  group.setAttribute("id", "set-bpm");
+  group.append(iconPath);
+
+  parent.append(group);
+}
+function createTimeSigIcon(parent: SVGElement) {
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
   const outerPath = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "path"
@@ -132,14 +164,19 @@ function createTimeSigIcon() {
     "http://www.w3.org/2000/svg",
     "path"
   );
+  const bg = document.querySelector("#bg-circle");
 
-  iconSvg.setAttribute("width", "48");
-  iconSvg.setAttribute("height", "48");
-  iconSvg.setAttribute("fill-rule", "evenodd");
-  iconSvg.setAttribute("clip-rule", "evenodd");
-  iconSvg.setAttribute("stroke-linejoin", "round");
-  iconSvg.setAttribute("stroke-miterlimit", "2");
+  const coords = bg?.getBoundingClientRect() as DOMRect;
+  const x = coords.x + X_OFFSET;
+  const y = coords.top + coords.height / 2 - X_OFFSET;
 
+  group.setAttribute("fill-rule", "evenodd");
+  group.setAttribute("clip-rule", "evenodd");
+  group.setAttribute("stroke-linejoin", "round");
+  group.setAttribute("stroke-miterlimit", "2");
+  group.setAttributeNS(null, "transform", `translate(${x},${y})`);
+
+  group.setAttribute("id", "time-sig");
   outerPath.setAttribute(
     "d",
     "M24 0c13.246 0 24 10.755 24 24 0 13.247-10.754 24-24 24S0 37.248 0 24C0 10.755 10.754 0 24 0zm0 6c9.934 0 18 8.066 18 18 0 9.935-8.066 18-18 18-9.935 0-18-8.065-18-18 0-9.934 8.065-18 18-18z"
@@ -149,8 +186,36 @@ function createTimeSigIcon() {
     "M25.008 11.76c1.504 0 2.936.312 4.296.936 1.36.624 2.472 1.472 3.336 2.544.864 1.072 1.296 2.28 1.296 3.624 0 1.184-.4 2.2-1.2 3.048-.8.848-1.808 1.272-3.024 1.272-1.088 0-1.984-.312-2.688-.936-.704-.624-1.056-1.48-1.056-2.568 0-.832.24-1.536.72-2.112a5.053 5.053 0 011.728-1.344c.352-.16.784-.296 1.296-.408.512-.112.768-.264.768-.456 0-.512-.448-.992-1.344-1.44a6.352 6.352 0 00-2.88-.672c-1.312 0-2.432.304-3.36.912-.928.608-1.624 1.648-2.088 3.12-.464 1.472-.696 3.52-.696 6.144v3.744c0 1.184.184 2.344.552 3.48.368 1.136.984 2.072 1.848 2.808.864.736 2 1.104 3.408 1.104 1.6 0 3.056-.72 4.368-2.16 1.312-1.44 2.16-3.376 2.544-5.808h1.296c-.128 1.856-.608 3.512-1.44 4.968s-1.952 2.6-3.36 3.432c-1.408.832-3.024 1.248-4.848 1.248-2.016 0-3.824-.584-5.424-1.752-1.6-1.168-2.864-2.728-3.792-4.68-.928-1.952-1.392-4.128-1.392-6.528 0-1.536.296-2.992.888-4.368a12.32 12.32 0 012.424-3.672 11.66 11.66 0 013.552-2.544 10.023 10.023 0 014.272-.936z"
   );
   innerPath.setAttribute("fill-rule", "nonzero");
+  group.append(outerPath);
+  group.append(innerPath);
+  parent.append(group);
+}
 
-  iconSvg.appendChild(outerPath);
-  iconSvg.appendChild(innerPath);
+function createBGsvg() {
+  const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const iconCircle = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  const center = getCenter();
+  const radius =
+    portOrLand() === "portrait"
+      ? window.innerWidth / 2
+      : window.innerHeight / 2;
+  const width = window.innerWidth.toString();
+  const height = window.innerHeight.toString();
+
+  iconSvg.setAttribute("width", `${width}`);
+  iconSvg.setAttribute("height", `${height}`);
+
+  iconCircle.setAttribute("fill", "grey");
+  iconSvg.setAttribute("id", "bg");
+  iconCircle.setAttribute("cx", `${center.x}`);
+  iconCircle.setAttribute("cy", `${center.y}`);
+  iconCircle.setAttribute("r", `${radius}`);
+  iconCircle.setAttribute("id", "bg-circle");
+  group.append(iconCircle);
+  iconSvg.appendChild(group);
   return iconSvg;
 }
